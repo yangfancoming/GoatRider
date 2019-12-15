fec7fbafe9636504ab3e704faf358fce49d758a4
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-15 18:26:30 +0800

    学习 LINQ 基础语法

--- chapter1-6-1/Program.cs
+++ chapter1-6-1/Program.cs
@@ -7,18 +7,28 @@ namespace chapter1_6_1
     {
         static void Main() {
             test1();
+//            test2();
         }
 
+        // Linq 语法 基础1
         public static void test1()        {
             var queryResults =
+                // 获取数据源 ( string[] )
                 from n in BaseData.names
+                // 定义查询条件(每条记录以字符串S开头)
                 where n.StartsWith("S")
+                // 返回结果
                 select n;
-
-            Log4C.log.Debug("Names beginning with S:");
             queryResults.ForEach(x => Log4C.log.Debug(x));
-            Log4C.log.Debug("Program finished ");
         }
+
+        // Linq 语法 基础2   简化方式
+        public static void test2()        {
+            // 在指定数据源中  定义查询条件(每条记录以字符串S开头)
+            var queryResults = BaseData.names.Where(n => n.StartsWith("S"));
+            queryResults.ForEach(x => Log4C.log.Debug(x));
+        }
+
 
     }
 }
