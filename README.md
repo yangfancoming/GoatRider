cb4f97c1bf7c9f53a308d66aa736e6f904e6d1fa
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-20 10:19:09 +0800

    增加 文件操作  I/O

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
     - chapter1-5-1  枚举器/迭代器
     - chapter1-6-1  LINQ
     - chapter1-7-1  集合
+    - chapter1-9-1  I/O
 
     - chapter2-1-1  设计模式
     - chapter2-5-1  office
---
+++ chapter1-9-1/Form1.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace chapter1_9_1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
