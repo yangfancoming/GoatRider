ecd8521084ee11739b1e567a9bd5838b38237973
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-01-15 11:05:31 +0800

    添加 quartz 使用示例  成功

--- chapter3-4-1/Program.cs
+++ chapter3-4-1/Program.cs
@@ -1,11 +1,81 @@
 ﻿
 
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Simpl;
+using Quartz.Xml;
+
 namespace chapter3_4_1 {
 
     class Program {
 
-        static void Main() {
+        public static void Main() {
+            Run().GetAwaiter().GetResult();
         }
 
+
+        private static async Task Run() {
+            var simpleTypeLoadHelper = new SimpleTypeLoadHelper();
+            XMLSchedulingDataProcessor processor = new XMLSchedulingDataProcessor(simpleTypeLoadHelper);
+
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+            await processor.ProcessFileAndScheduleJobs("E:\\Code\\C#_code\\RiderLearning\\GoatRider\\chapter3-4-1\\quartz_jobs.xml", scheduler);
+            await scheduler.Start();
+            await Task.Delay(TimeSpan.FromSeconds(20));
+            await scheduler.Shutdown();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
